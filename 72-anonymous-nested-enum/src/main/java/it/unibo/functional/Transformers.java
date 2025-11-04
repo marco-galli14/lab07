7a931f61a3b0cbd66860b60b91448a5f1da487f4
Marco <marco.galli14@studio.unibo.it>
2025-11-04 15:19:19 +0100

    Risolte parti 1 e 2 esercizio 72

--- 72-anonymous-nested-enum/src/main/java/it/unibo/functional/Transformers.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/functional/Transformers.java
@@ -4,6 +4,7 @@ import it.unibo.functional.api.Function;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
 
@@ -54,7 +55,11 @@ public final class Transformers {
      * @return A transformed list where each input element is replaced with the produced elements
      */
     public static <I, O> List<O> transform(final Iterable<I> base, final Function<I, O> transformer) {
-        return null;
+        var ris = new ArrayList<O>();
+        for (final I input : base) {
+            ris.add(transformer.call(input));
+        }
+        return ris;
     }
 
     /**
@@ -70,7 +75,9 @@ public final class Transformers {
      * @return A flattened list with the elements of each collection in the input
      */
     public static <I> List<? extends I> flatten(final Iterable<? extends Collection<? extends I>> base) {
-        return null;
+        var ris = new ArrayList<I>();
+        ris.addAll(flattenTransform(base, Function.identity()));
+        return ris;
     }
 
     /**
@@ -87,7 +94,18 @@ public final class Transformers {
      * @return A list containing only the elements that passed the test
      */
     public static <I> List<I> select(final Iterable<I> base, final Function<I, Boolean> test) {
-        return null;
+        var ris = new ArrayList<I>();
+        Iterator<I> it = base.iterator();
+        I a;
+        a = it.next();
+        for(final Boolean elem : transform(base, test)) {
+            if(elem){
+                ris.add(a);
+            }
+            if(it.hasNext())
+                a = it.next();
+        }
+        return ris;
     }
 
     /**
@@ -103,6 +121,17 @@ public final class Transformers {
      * @return A list containing only the elements that passed the test
      */
     public static <I> List<I> reject(final Iterable<I> base, final Function<I, Boolean> test) {
-        return null;
+        var ris = new ArrayList<I>();
+        Iterator<I> it = base.iterator();
+        I a;
+        a = it.next();
+        for(final Boolean elem : transform(base, test)) {
+            if(!elem){
+                ris.add(a);
+            }
+            if(it.hasNext())
+                a = it.next();
+        }
+        return ris;
     }
 }
--- 72-anonymous-nested-enum/src/main/java/it/unibo/functional/api/Function.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/functional/api/Function.java
@@ -24,7 +24,9 @@ public interface Function<I, O> {
      * @return A new {@code Function} that implements the identity function with the provided type
      */
     static <T> Function<T, T> identity() {
-        return null;
+        return new Function<T,T>() {
+            public T call(T input) {return input;}
+        };
     }
 
 }
